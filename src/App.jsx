7db049d30f75c10e9b8f0d7e03b43362439a25f5
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-04 02:35:06 +0300

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Analytics from "./components/Analytics";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
@@ -12,6 +13,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer />
     </>
   );
 }
