153c5347fe5635c6c14ec5a1089f9bbfc7a12ffe
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-04 02:08:51 +0300

    add cards section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
     </>
   );
 }
