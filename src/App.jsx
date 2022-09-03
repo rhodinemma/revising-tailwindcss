7ce84047f79b20e60554b8357b120a9a32bd54fb
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-03 21:54:05 +0300

    configure navbar and font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Navbar />;
 }
 
 export default App;
